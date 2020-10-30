3fd9526cabf5157b873f06ebbeb4ec80da0f2894
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2020-10-30 12:35:04 +0200

    Apply custom firefox profile configuration

--- rciam_probes/probes/checkhealth/checkhealth.py
+++ rciam_probes/probes/checkhealth/checkhealth.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 from rciam_probes.shared.authentication import *
 # import methods from the lib directory
@@ -23,6 +24,7 @@ class RciamHealthCheck:
     __last_url = None
     __cached_cookies = None
     __options = None
+    __profile = None
     __wait = None
     __start_time = None
     __args = None
@@ -47,17 +49,22 @@ class RciamHealthCheck:
         self.__options.accept_insecure_certs = True
         self.__geckodriver_binary = self.__args.geckodriver
         self.__firefox_binary = FirefoxBinary(self.__args.firefox)
+        # Set firefox profile
+        self.__profile = webdriver.FirefoxProfile()
+        firefox_profile(self.__profile)
         if self.__browser is not None:
             self.__browser.close()
 
         if(self.__args.console):
             self.__browser = webdriver.Firefox(options=self.__options,
                                                firefox_binary=self.__firefox_binary,
+                                               firefox_profile=self.__profile,
                                                executable_path=self.__geckodriver_binary,
                                                log_path=os.path.devnull)
         else:
             self.__browser = webdriver.Firefox(options=self.__options,
                                                firefox_binary=self.__firefox_binary,
+                                               firefox_profile=self.__profile,
                                                executable_path=self.__geckodriver_binary,
                                                log_path=self.__args.log)
 
@@ -351,6 +358,45 @@ def parse_arguments(args):
 
     return parser.parse_args(args)
 
+def firefox_profile(firefox_profile):
+    """
+    Apply firefox profile configuration
+    :param firefox_profile: firefox geckodriver webdriver.FirefoxProfile()
+    :type firefox_profile:  FirefoxProfile object
+    """
+    firefox_profile.set_preference("network.http.pipelining", True)
+    firefox_profile.set_preference("network.http.proxy.pipelining", True)
+    firefox_profile.set_preference("network.http.pipelining.maxrequests", 8)
+    firefox_profile.set_preference("content.notify.interval", 500000)
+    firefox_profile.set_preference("content.notify.ontimer", True)
+    firefox_profile.set_preference("content.switch.threshold", 250000)
+    firefox_profile.set_preference("browser.cache.memory.capacity", 65536)  # Increase the cache capacity.
+    firefox_profile.set_preference("browser.startup.homepage", "about:blank")
+    firefox_profile.set_preference("reader.parse-on-load.enabled", False)  # Disable reader, we won't need that.
+    firefox_profile.set_preference("browser.pocket.enabled", False)  # Duck pocket too!
+    firefox_profile.set_preference("loop.enabled", False)
+    firefox_profile.set_preference("browser.chrome.toolbar_style", 1)  # Text on Toolbar instead of icons
+    firefox_profile.set_preference("browser.display.show_image_placeholders", False)  # Don't show thumbnails on not loaded images.
+    firefox_profile.set_preference("browser.display.use_document_colors", False)  # Don't show document colors.
+    firefox_profile.set_preference("browser.display.use_document_fonts", 0)  # Don't load document fonts.
+    firefox_profile.set_preference("browser.display.use_system_colors", True)  # Use system colors.
+    firefox_profile.set_preference("browser.formfill.enable", False)  # Autofill on forms disabled.
+    firefox_profile.set_preference("browser.helperApps.deleteTempFileOnExit", True)  # Delete temprorary files.
+    firefox_profile.set_preference("browser.shell.checkDefaultBrowser", False)
+    firefox_profile.set_preference("browser.startup.homepage", "about:blank")
+    firefox_profile.set_preference("browser.startup.page", 0)  # blank
+    firefox_profile.set_preference("browser.tabs.forceHide", True)  # Disable tabs, We won't need that.
+    firefox_profile.set_preference("browser.urlbar.autoFill", False)  # Disable autofill on URL bar.
+    firefox_profile.set_preference("browser.urlbar.autocomplete.enabled", False)  # Disable autocomplete on URL bar.
+    firefox_profile.set_preference("browser.urlbar.showPopup", False)  # Disable list of URLs when typing on URL bar.
+    firefox_profile.set_preference("browser.urlbar.showSearch", False)  # Disable search bar.
+    firefox_profile.set_preference("extensions.checkCompatibility", False)  # Addon update disabled
+    firefox_profile.set_preference("extensions.checkUpdateSecurity", False)
+    firefox_profile.set_preference("extensions.update.autoUpdateEnabled", False)
+    firefox_profile.set_preference("extensions.update.enabled", False)
+    firefox_profile.set_preference("general.startup.browser", False)
+    firefox_profile.set_preference("plugin.default_plugin_disabled", False)
+    firefox_profile.set_preference("permissions.default.image", 2)
 
 # Entry point
 if __name__ == "__main__":
